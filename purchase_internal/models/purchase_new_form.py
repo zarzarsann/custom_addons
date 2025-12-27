94f86aa225393bf26e55a1bc91b2ae0479d7c1da
Zar Zar <zarzarsan@sssretail.com>
2025-12-27 19:19:20 +0630

    fixed purchase internal module

--- purchase_internal/models/purchase_new_form.py
+++ purchase_internal/models/purchase_new_form.py
@@ -1,10 +1,12 @@
 from odoo import models, fields
 
 class MyModel(models.Model):
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _name = 'purchase.new'
     _description = 'Purchase New'
 
-    name = fields.Char(string="Name", required=True)
+    name = fields.Char(string="Name", required=True, tracking=True)
+    partner_id = fields.Many2one(comodel_name='res.partner', required=True)
     description = fields.Text(string="Description")
     active = fields.Boolean(default=True)
 
